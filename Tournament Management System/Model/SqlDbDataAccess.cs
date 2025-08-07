de935668ec43d1beac3561e85397d53fd0ec1de4
ABDULLAH AL NAHIN <abdullahalnahin6@gmail.com>
2025-08-08 02:24:57 +0600

    Tms

---
+++ Tournament Management System/Model/SqlDbDataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Tournament_Management_System.Model
+{
+    public class SqlDbDataAccess
+    {
+        
+        private const string connectionString = @"Data Source=DESKTOP-VRGVRI4\SQLEXPRESS; Initial Catalog=OnlineTournamentDB; Integrated Security=True";
+        public SqlCommand GetQuery(string query)
+        {
+            var connection = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Connection = connection;
+
+            return cmd;
+        }
+    }
+}
